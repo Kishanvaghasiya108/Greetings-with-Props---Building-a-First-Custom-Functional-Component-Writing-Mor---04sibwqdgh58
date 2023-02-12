eb82f0517a7527aa3a3c6cfeee5c8076b7a93e4e
Kishanvaghasiya108 <122118895+Kishanvaghasiya108@users.noreply.github.com>
2023-02-12 15:29:57 +0530

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  let name = "Adarsh sharma";
   return (
-    <Welcome name="" />
+    <Welcome name={name} />
   )
 }
 
